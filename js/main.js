87e105a91dbf915177f228df85f039e195bf7a2e
Samuel Jägestedt <ichbinjaeger@gmail.com>
2021-03-07 14:04:07 +0100

    add static dialog, add styling

--- js/main.js
+++ js/main.js
@@ -2,10 +2,49 @@ $(() => {
   console.log('jQ rdy');
 });
 
+// SORTABLE
 $(function () {
   $('#sortable1, #sortable2, #sortable3')
     .sortable({
       connectWith: '.connectedSortable',
     })
     .disableSelection();
+});
+
+// DIALOG 1
+$(function () {
+  $('#dialog-1').dialog({
+    autoOpen: false,
+    show: {
+      effect: 'fade',
+      duration: 200,
+    },
+    hide: {
+      effect: 'fade',
+      duration: 200,
+    },
+  });
+
+  $('#opener-1').on('click', function () {
+    $('#dialog-1').dialog('open');
+  });
+});
+
+// DIALOG 2
+$(function () {
+  $('#dialog-2').dialog({
+    autoOpen: false,
+    show: {
+      effect: 'fade',
+      duration: 200,
+    },
+    hide: {
+      effect: 'fade',
+      duration: 200,
+    },
+  });
+
+  $('#opener-2').on('click', function () {
+    $('#dialog-2').dialog('open');
+  });
 });
