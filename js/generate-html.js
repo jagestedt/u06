7b9e87109ae8003908d9849c2b6d939119a08d9e
ichbinjaeger <ichbinjaeger@users.noreply.github.com>
2021-03-10 18:46:00 +0100

    liveshare

---
+++ js/generate-html.js
@@ -0,0 +1,28 @@
+$(() => {
+  let list = $('#todo');
+  for (let i = 1; i < 6; i++) {
+    let cards = `<li id="task-${i}" class="p-2 m-2 card card-background"><h3 class="title is-6">Item ${i}</h3> 
+            <div id="tabs" class="tabs my-tabs">
+              <ul>
+                <li><a href="#tabs-1">First Man</a></li>
+                <li><a href="#tabs-2">Second breakfast</a></li>
+                <li><a href="#tabs-3">Third Man</a></li>
+              </ul>
+              <section id="tabs-1" class="has-background-success p-2">
+                <p>1</p>
+              </section>
+              <section id="tabs-2" class="has-background-primary p-2">
+                <p>2</p>
+              </section>
+              <section id="tabs-3" class="has-background-warning p-2">
+                <p>3</p>
+              </section>
+            </div>
+            <label for="datepicker-${i}">Pick a deadline:</label> 
+            <input type="text" id="datepicker-${i}">
+            <p>Current deadline: <input class="has-text-dark mt-4 mb-4" type="text" id="deadline-${i}" size="30"></p>
+            <button id="opener-${i}" class="button">Details</button>
+          </li>`;
+    list.append(cards);
+  }
+});
--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(() => {
 
   // DIALOG
   let numberOfCards = $('.task-list').children().length;
-  console.log(numberOfCards);
+  //   console.log('Contains: ' + numberOfCards + ' tasks');
 
   function createDialogWidgets() {
     for (let i = 0; i < numberOfCards + 1; i++) {
@@ -25,7 +25,6 @@ $(() => {
           duration: 200,
         },
       });
-
       $(`#opener-${i}`).on('click', function () {
         $(`#dialog-${i}`).dialog('open');
       });
@@ -37,33 +36,61 @@ $(() => {
   const dialogContainer = $('.dialog-container');
 
   for (let i = 0; i < numberOfCards + 1; i++) {
-    dialogElement = `<div id="dialog-${i}" class="has-background-light" title="Static dialog">
+    dialogElement = `<div id="dialog-${i}" class="dialog has-background-light" title="Static dialog">
+    <div id="tabs" class="tabs my-tabs">
+              <ul>
+                <li><a href="#tabs-1">First Man</a></li>
+                <li><a href="#tabs-2">Second breakfast</a></li>
+              </ul>
+              <section id="tabs-1" class="has-background-success p-2">
+                <p>1</p>
+              </section>
+              <section id="tabs-2" class="has-background-primary p-2">
+                <p>2</p>
+              </section>
+            </div>
             <p class="dialog-text">Info about task ${i}</p>
         </div>`;
     dialogContainer.append(dialogElement);
   }
 
+  //   $(function () {
+  //     $('.dialog').dialog({
+  //       autoOpen: false,
+  //       show: {
+  //         effect: 'fade',
+  //         duration: 200,
+  //       },
+  //       hide: {
+  //         effect: 'fade',
+  //         duration: 200,
+  //       },
+  //     });
+  //     $('.btn').on('click', function () {
+  //       let id = $(this).attr('data-id');
+  //       $(id).dialog('open');
+  //     });
+  //   });
+
   createDialogWidgets();
 
   // TABS
-  $('#tabs').tabs();
+  //   $('#tabs').tabs();
+  $('.dialog').tabs();
 
-  // DATEPICKER
-  $('#datepicker').datepicker({
-    altField: '#deadline',
-    altFormat: 'DD, d MM, yy',
-    showOtherMonths: true,
-    selectOtherMonths: false,
-  });
+  for (let i = 1; i < 6; i++)
+    // DATEPICKER
+    $('#datepicker').datepicker({
+      altField: '#deadline',
+      altFormat: 'DD, d MM, yy',
+      showOtherMonths: true,
+      selectOtherMonths: false,
+    });
+
   // Generalisera, lägg till mouseup-event
-
   // WIDGET
   let taskCounterElement = $('#task-counter');
   const ulElement = $('.task-list');
-
-  $('.card').mouseup(function () {
-    console.log('mouseUP');
-  });
 
   $.widget('u06.taskCounter', {
     options: {
@@ -72,7 +99,6 @@ $(() => {
 
     _create: function () {
       this.element.addClass('taskCounter');
-
       this._update();
     },
 
@@ -82,24 +108,83 @@ $(() => {
     },
     // uppdaterar elementet
     _update: function () {
-      let listLength = ulElement.children().length;
-      const text = `Contains: ${listLength} tasks`;
-      this.element.text(text);
-    },
-
-    value: function (value) {
-      if (value === undefined) {
-        return this.options.value;
-      } else {
-        this.options.value = this._constrain(value);
-        this._update();
-      }
+      console.log('update');
+      $('.card').mouseup(function () {
+        let listLength = ulElement.children().length;
+        const output = `Contains: ${listLength} tasks`;
+        console.log(output);
+        this.element.append(output);
+      });
     },
 
     _destroy: function () {
-      this.element.removeClass('taskCounter').text('');
+      this.element.removeClass('taskCounter');
     },
   });
 
   taskCounterElement.taskCounter();
 });
+//   let taskCounterElement = $('#task-counter');
+//   const ulElement = $('.task-list');
+
+//   $('#theme-switch').click(() => {
+//     if ($(this).prop('checked') == true) {
+//       console.log('checked');
+//     } else {
+//       console.log('un-checked');
+//     }
+//   });
+
+//   $('#theme-switch').click(function () {
+//     if ($(this).prop('checked') == true) {
+//       console.log('Checkbox is checked.');
+//       $('body').addClass('dark-mode');
+//     } else {
+//       console.log('Checkbox is unchecked.');
+//       $('body').removeClass('dark-mode');
+//     }
+//   });
+
+//   //   $('.card').mouseup().taskCounter();
+
+//   $.widget('u06.themeSwitch', {
+//     options: {
+//       value: false,
+//     },
+
+//     _create: function () {
+//       this.element.addClass('dark-mode');
+//       console.log(this);
+//       this._update();
+//     },
+
+//     _setOption: function (key, value) {
+//       this.options[key] = value;
+//       this._update();
+//     },
+//     // uppdaterar elementet
+//     _update: function () {
+//       $('.card').mouseup(function () {
+//         let listLength = ulElement.children().length;
+//         const text = `Contains: ${listLength} tasks`;
+//         console.log(this);
+//         // this.element.text(text);
+//       });
+//     },
+
+//     value: function (value) {
+//       if (value === undefined) {
+//         return this.options.value;
+//       } else {
+//         this.options.value = this._constrain(value);
+//         this._update();
+//       }
+//     },
+
+//     _destroy: function () {
+//       this.element.removeClass('.dark-mode');
+//     },
+//   });
+//   $('#theme-switch').click().themeSwitch();
+//   taskCounterElement.taskCounter();
+// });
